f1be9bcb529e8b57edfbe5a33bdbecddafd38845
Javier0003 <jpere0500@gmail.com>
2023-10-28 18:23:47 +0200

    mayor changes useState variables new functiones for: -timer -current animation frame fixed css errors with the grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-// import { useState } from 'react'
 import { LeftPannel } from './component/leftPannel'
 import styles from './styles/body.module.css'
 import { TopPannel } from './component/topPannel'
+import { Canvas } from './component/canvas'
+import { useState } from 'react'
 
 const players = [
   {
@@ -14,14 +15,29 @@ const players = [
     score: 420,
     color: 'black',
   },
+  {
+    name: 'jew',
+    score: 50,
+    color: 'rgb(255, 255, 69)',
+  },
 ]
 
+const animationTimer = 100
+
 function App() {
-  // const { players, setPlayers } = useState([])
+  const [currentValue, setCurrentValue] = useState(0)
+
   return (
     <div className={styles.container}>
-      <LeftPannel players={players} />
-      <TopPannel />
+      <LeftPannel players={players} round={currentValue} />
+      <div className={styles.sidePannel}>
+        <TopPannel
+          setCurrentValue={setCurrentValue}
+          startingValue={currentValue}
+          timer={animationTimer}
+        />
+        <Canvas />
+      </div>
     </div>
   )
 }
