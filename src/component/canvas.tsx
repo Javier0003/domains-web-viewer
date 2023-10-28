c9c2f451f41c0731f8ad97b3533e28f7eb3b2b2b
Javier0003 <jpere0500@gmail.com>
2023-10-28 18:24:02 +0200

    canvas

---
+++ src/component/canvas.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/canvas.module.css'
+
+export function Canvas() {
+  return (
+    <div className={styles.container}>
+      {/* <canvas className={styles.canvas}></canvas> */}
+    </div>
+  )
+}
